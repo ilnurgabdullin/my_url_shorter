b5324cb5cc7f1aa21e8231ec961b58334815567d
ilnur <ilnurgabdullin627@gmail.com>
2024-07-10 00:04:01 +0300

    connect to db

--- main.go
+++ main.go
@@ -3,14 +3,40 @@ package main
 import (
 	"github.com/gin-gonic/gin"
     "url_shorter/handlers"
+    "database/sql"
+    "fmt"
+    _ "github.com/lib/pq"
 	)
+
+const (
+    host = "192.168.1.8"
+    port = 5432
+    user = "postgres"
+    password = "123"
+    dbname = "baza"
+)
+
 
 func main() {
 	r:= gin.Default()
 
 	r.GET("/status", handlers.StatusCheck)
 	r.POST("/short", handlers.ShortUrl)
-
+    
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        fmt.Println("=================================")
+        fmt.Println(err)
+        return
+    }
+    
+    defer db.Close()
+    err = db.Ping()
+    if err != nil {
+            fmt.Println("ERROR 2")
+    }
+    fmt.Println("Succes")
     r.Run(":8080")
 
 }
